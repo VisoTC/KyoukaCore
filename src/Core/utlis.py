e06aec53c8728d17fd19efe628cde05e7abbfab0
Viso-TC <szlytlyt@outlook.com>
2020-09-02 17:29:22 +0800

    新增：帮助类：只读属性类

---
+++ src/Core/utlis.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+
+class ReadOnly():
+    """
+    只读类，执行 self.readOnlyLock() 后将会屏蔽写入操作
+    """
+    __readOnlyLock = False
+
+    def readOnlyLock(self):
+        self.__readOnlyLock = True
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if self.__readOnlyLock:
+            raise AttributeError('{}.{} is READ ONLY'.
+                                 format(type(self).__name__, name))
+        else:
+            return super().__setattr__(name, value)
