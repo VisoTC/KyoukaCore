495b556baa9cfcb4381d3b7a8d46cce06be693d3
Viso-TC <szlytlyt@outlook.com>
2020-09-02 13:37:33 +0800

    更新：更新通配符方法，现在通配符不再局限于*代表全部了

--- src/Core/bus/__init__.py
+++ src/Core/bus/__init__.py
@@ -4,6 +4,8 @@ from typing import Dict, List
 from ..Event import Eventer, EventerType, ReceiveEvent, SendEvent
 import threading
 
+from fnmatch import fnmatch
+
 
 class BUS():
     """
@@ -19,7 +21,7 @@ class BUS():
             EventerType.Core: []
         }
         BUSThread = threading.Thread(
-            target=self.threadMain, name="KyoukaCore event BUS loop thread",daemon=True)
+            target=self.threadMain, name="KyoukaCore event BUS loop thread", daemon=True)
         BUSThread.start()
 
     def threadMain(self):
@@ -27,7 +29,7 @@ class BUS():
             kyoukaMsg = self._sendBus.get(block=True)  # 从发送总线中获取事件
             for terger in kyoukaMsg.terger:  # 判断事件接收者类型
                 for port in self._ports[terger.type]:  # 获得接收者端口
-                    if terger.name == "*" or terger.name == port.eventer.name:  # 判断是否符合，不符合就跳过
+                    if fnmatch(port.eventer.name, terger.name):  # 判断是否符合，不符合就跳过
                         port._BUSPort__BUSsend(ReceiveEvent(
                             kyoukaMsg.source, kyoukaMsg.payload))  # 重新组装成为接收者事件并发送事件
                     else:
