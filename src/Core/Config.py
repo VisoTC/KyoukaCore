e2f34cd680a48219bf703a0d6cba67a8b83ae4c8
Viso-TC <szlytlyt@outlook.com>
2020-09-13 21:04:05 +0800

    新增：配置类

---
+++ src/Core/Config.py
@@ -0,0 +1,92 @@
+# 统一的配置管理类
+import collections
+import logging
+import os
+import sys
+import json
+import copy
+import threading
+
+
+class ConfigFactory():
+    _instance_lock = threading.Lock()
+    __isInit = False
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(ConfigFactory, "_instance"):
+            with ConfigFactory._instance_lock:
+                if not hasattr(ConfigFactory, "_instance"):
+                    ConfigFactory._instance = super(
+                        ConfigFactory, cls).__new__(cls)
+        return ConfigFactory._instance
+
+    def __init__(self) -> None:
+        if not self.__isInit:
+            self.logger = logging.getLogger("KyoukaCore Config")
+            self.filePath = os.path.join(
+                sys.path[0], "config.json")
+            if os.path.exists(self.filePath):
+                with open(self.filePath) as configFile:
+                    tmpConfig: dict = json.loads(configFile.read())
+                    if not "KyoukaBot" in tmpConfig.keys():
+                        tmpConfig = self.initConfig()
+                    if tmpConfig["KyoukaBot"]['configVersion'] != "1":
+                        # 可在此进行配置版本升级操作，若版本无法处理直接初始化
+                        tmpConfig = self.initConfig()
+                        self.save()
+                    self.__config = tmpConfig
+            else:
+                self.__config = self.initConfig()
+                self.save()
+            self.__isInit = True
+
+    def initConfig(self):
+        self.logger.info("Init config...")
+        return {
+            "KyoukaBot": {
+                "configVersion": "1",
+                "LogLevel": "info",
+                "adminUser": ""
+            }
+        }
+
+    def getConfig(self, packageName):
+        if not packageName in self.__config.keys():
+            return dict()
+        else:
+            return copy.deepcopy(self.__config[packageName])
+
+    def save(self):
+        self.logger.info("Save config...")
+        with open(self.filePath, 'w') as cfile:
+            cfile.write(json.dumps(self.__config,
+                                   indent=4, ensure_ascii=False))
+
+    def commit(self, config):
+        self.__config[config.packageName] = copy.deepcopy(config.data)
+        self.save()
+
+
+class Config(collections.UserDict):
+    def __init__(self, packageName: str) -> None:
+        """
+        配置文件
+        :param packageName: 包名
+        """
+        self.__packageName = packageName
+        self.__configRoot = ConfigFactory()
+        self.data = self.__configRoot.getConfig(self.__packageName)
+
+    @property
+    def packageName(self):
+        return self.__packageName
+
+    @property
+    def isEmpty(self):
+        return len(self.data.keys()) == 0
+
+    def commit(self):
+        """
+        提交更改到配置，必须执行此方法才能同步修改到系统配置核心
+        """
+        self.__configRoot.commit(self)
